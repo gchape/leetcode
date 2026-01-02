411d84b65fddec5e89574b4f4a13f6fc335e830f
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-01-02 11:06:18 +0400

    Fix AVL rebalancing and height maintenance

--- src/algs/avl.clj
+++ src/algs/avl.clj
@@ -16,57 +16,45 @@
     :right nil})
   ([key left right]
    {:key key
-    :height (-> (max (height left)
-                     (height right))
-                (inc))
+    :height (inc (max (height left) (height right)))
     :left left
     :right right}))
 
-(defn- rotate-left
-  [tree]
+(defn- rotate-left [tree]
   (let [r (:right tree)]
     (node
      (:key r)
-     (node
-      (:key tree)
-      (:left tree)
-      (:left r))
+     (node (:key tree) (:left tree) (:left r))
      (:right r))))
 
-(defn- rotate-right
-  [tree]
+(defn- rotate-right [tree]
   (let [l (:left tree)]
     (node
      (:key l)
      (:left l)
-     (node
-      (:key tree)
-      (:right l)
-      (:right tree)))))
+     (node (:key tree) (:right l) (:right tree)))))
 
-(defn- balance
-  [tree]
-  (- (height (:right  tree))
+(defn- balance [tree]
+  (- (height (:right tree))
      (height (:left tree))))
 
-(defn- fix-balance
-  [tree]
-  (let [blnc (balance tree)]
+(defn- fix-balance [tree]
+  (let [b (balance tree)]
     (cond
-      (> blnc 1)
-      (let [r (:right tree)
-            blnc (balance r)]
-        (if (< blnc 0)
-          (rotate-left
-           (assoc tree :right (rotate-right r)))
+      (> b 1)
+      (let [r (:right tree)]
+        (if (< (balance r) 0)
+          ;; Right-Left case
+          (rotate-left (node (:key tree) (:left tree) (rotate-right r)))
+          ;; Right-Right case
           (rotate-left tree)))
 
-      (< blnc -1)
-      (let [l (:left tree)
-            blnc (balance l)]
-        (if (< blnc 0)
-          (rotate-right
-           (assoc tree :left (rotate-left l)))
+      (< b -1)
+      (let [l (:left tree)]
+        (if (> (balance l) 0)
+          ;; Left-Right case
+          (rotate-right (node (:key tree) (rotate-left l) (:right tree)))
+          ;; Left-Left case
           (rotate-right tree)))
 
       :else
@@ -78,80 +66,83 @@
     (node k)
 
     (< k (:key tree))
-    (fix-balance
-     (node (:key tree)
-           (insert- (:left tree) k)
-           (:right tree)))
+    (fix-balance (node (:key tree)
+                       (insert- (:left tree) k)
+                       (:right tree)))
 
     (> k (:key tree))
-    (fix-balance
-     (node (:key tree)
-           (:left tree)
-           (insert- (:right tree) k)))
+    (fix-balance (node (:key tree)
+                       (:left tree)
+                       (insert- (:right tree) k)))
 
-    :else tree))
+    :else tree)) ; no duplicates
 
 (defn- leftmost-node [tree]
-  (if (nil? (:left tree))
-    tree
-    (recur (:left tree))))
+  (if (:left tree)
+    (recur (:left tree))
+    tree))
 
-(defn- remove-
-  [tree k]
+(defn- remove- [tree k]
   (cond
     (nil? tree)
     nil
 
     (< k (:key tree))
-    (fix-balance
-     (assoc tree :left
-            (remove- (:left tree) k)))
+    (fix-balance (node (:key tree)
+                       (remove- (:left tree) k)
+                       (:right tree)))
 
     (> k (:key tree))
-    (fix-balance
-     (assoc tree :right
-            (remove- (:right tree) k)))
+    (fix-balance (node (:key tree)
+                       (:left tree)
+                       (remove- (:right tree) k)))
 
     :else
+    ;; node to delete found
     (cond
-      (and (nil? (:left tree))
-           (nil? (:right tree)))
+      ;; no children
+      (and (nil? (:left tree)) (nil? (:right tree)))
       nil
 
+      ;; one child
       (nil? (:left tree))
       (:right tree)
 
       (nil? (:right tree))
       (:left tree)
 
+      ;; two children
       :else
       (let [succ (leftmost-node (:right tree))]
         (fix-balance
-         (assoc tree
-                :key
-                succ
-                :right
-                (remove- (:right tree) (:key succ))))))))
+         (node (:key succ)
+               (:left tree)
+               (remove- (:right tree) (:key succ))))))))
 
 (defn find [tree k]
   (cond
-    (nil? tree)
-    nil
-
-    (< k (:key tree))
-    (find (:left tree) k)
-
-    (> k (:key tree))
-    (find (:right tree) k)
-
-    :else
-    (:key tree)))
+    (nil? tree) nil
+    (< k (:key tree)) (find (:left tree) k)
+    (> k (:key tree)) (find (:right tree) k)
+    :else (:key tree)))
 
 (defn insert [tree k]
-  (let [new-tree (insert- tree k)]
-    (fix-balance new-tree)))
+  (insert- tree k))
 
-(defn remove
-  [tree k]
-  (fix-balance
-   (remove- tree k)))
+(defn remove [tree k]
+  (remove- tree k))
+
+(inspector/inspect-tree
+ (-> nil
+     (insert 10)
+     (insert 5)
+     (insert 15)
+     (insert 3)
+     (insert 7)
+     (insert 12)
+     (insert 18)
+     (insert 6)
+     (insert 8)
+     (remove 15)
+     (remove 3)
+     (remove 10)))
