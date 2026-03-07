04dce887feecf9680fc35e26d09a6f846414c6d3
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-03-07 22:22:14 +0400

    Time: 0 ms (100.00%) | Memory: 47.4 MB (65.33%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1 = 0, xor2 = 0;
+        
+        for (var n : nums) {
+            xor1 ^= n;
+        }
+
+        for (int i = 0; i <= nums.length; i++) {
+            xor2 ^= i;
+        }
+
+        return xor1 ^ xor2;
+    }
+}
