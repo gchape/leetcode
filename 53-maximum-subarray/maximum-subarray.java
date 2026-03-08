ac8596087176213bcb140499db20eae9e7a0e8a1
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-03-08 17:34:00 +0400

    Time: 1 ms (99.80%) | Memory: 77.4 MB (30.25%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        long max = Long.MIN_VALUE, sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (sum > max) {
+                max = sum;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return (int) max;
+    }
+}
