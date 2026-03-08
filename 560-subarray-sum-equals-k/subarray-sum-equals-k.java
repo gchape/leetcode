56e0d937db7546a22aa204eab0369df87a391db6
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-03-08 16:51:11 +0400

    Time: 20 ms (98.06%) | Memory: 48.7 MB (69.52%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,22 +1,24 @@
-import java.util.HashMap;
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        int prefixSum = 0;
-        Map<Integer, Integer> prefixSumCount = new HashMap<>();
-        
-        prefixSumCount.put(0, 1);
-        for (int num : nums) {
-            prefixSum += num;
-            
-            if (prefixSumCount.containsKey(prefixSum - k)) {
-                count += prefixSumCount.get(prefixSum - k);
+        int sum = 0, count = 0;
+        var prefix = new HashMap<Integer, Integer>() {
+            {
+                put(0, 1);
             }
-            
-            prefixSumCount.put(prefixSum, prefixSumCount.getOrDefault(prefixSum, 0) + 1);
+        };
+
+        for (var n : nums) {
+            sum += n;
+
+            if (prefix.containsKey(sum - k)) {
+                count += prefix.get(sum - k);
+            }
+
+            prefix.compute(
+                    sum,
+                    (_, v) -> v == null ? 1 : v + 1);
         }
-        
+
         return count;
     }
 }
