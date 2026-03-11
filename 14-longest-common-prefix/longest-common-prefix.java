ec142bfdb511191ac07024fc22eba9330f2b32c6
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-03-11 21:58:51 +0400

    Time: 1 ms (65.04%) | Memory: 43.1 MB (81.78%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,26 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 1) {
-            return strs[0];
+        if (strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
         }
 
-        Arrays.sort(strs);
-
-        String first = strs[0];
-        String last = strs[strs.length - 1];
-
-        return join(first, last, 0);
-    }
-
-    private String join(String first, String last, int i) {
-        if (i == first.length() || i == last.length()) {
-            return "";
-        }
-
-        if (first.charAt(i) == last.charAt(i)) {
-            return first.charAt(i) + "" + join(first, last, i + 1);
-        }
-
-        return "";
+        return strs[0];
     }
 }
