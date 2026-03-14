18b96b44cd5a1b032e83fbbb2707d3556bfd5561
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-03-14 20:22:42 +0400

    Time: 0 ms (100.00%) | Memory: 42.8 MB (55.12%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,13 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        var temp = new StringBuilder(s);
-        for (int i = 0; i < s.length(); i++) {
-            if (temp.toString().equals(goal)) {
-                return true;
-            }
-            temp.append(temp.charAt(0)).deleteCharAt(0);
-        }
-
-        return false;
+        return s.length() == goal.length() && (s + s).contains(goal);
     }
 }
