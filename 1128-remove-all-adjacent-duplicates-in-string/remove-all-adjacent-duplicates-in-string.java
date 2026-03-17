fa8d5d5b06e8d724346ccb033ea6142be23c7a5c
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-03-17 22:23:31 +0400

    Time: 14 ms (74.21%) | Memory: 46.3 MB (96.26%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        if (s.length() == 1) return s;
+
+        var stack = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int size = stack.length();
+
+            if (size > 0 && stack.charAt(size - 1) == s.charAt(i)) {
+                stack.deleteCharAt(size - 1);
+            } else {
+                stack.append(s.charAt(i));
+            }
+        }
+
+        return stack.toString();
+    }
+}
