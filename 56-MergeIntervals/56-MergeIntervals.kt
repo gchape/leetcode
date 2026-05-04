aac8ef029c7920f209d12c0566d8a819e44396f9
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 20:38:50 +0400

    File updated at 5/4/2026, 8:38:50 PM

--- 56-MergeIntervals/56-MergeIntervals.kt
+++ 56-MergeIntervals/56-MergeIntervals.kt
@@ -1,30 +1,19 @@
-// Last updated: 5/4/2026, 8:09:04 PM
+// Last updated: 5/4/2026, 8:38:50 PM
 1class Solution {
-2    fun removeElement(nums: IntArray, `val`: Int): Int {
-3               for ((i, element) in nums.withIndex()) {
-4        if (element == `val`) {
-5            nums[i] = -1
-6        }
-7    }
-8
-9    var j: Int? = null
-10    var i = 0
-11
-12    while (i < nums.size) {
-13        if (nums[i] == -1 && j == null) {
-14            j = i
-15        }
-16
-17        if (nums[i] != -1 && j != null) {
-18            nums[j] = nums[i]
-19            nums[i] = -1
-20            i = j
-21            j = null
-22        }
-23
-24        i++
-25    }
-26
-27    return j ?: nums.size
-28    }
-29}
+2    fun strStr(haystack: String, needle: String): Int {
+3        for (i in 0..haystack.length - needle.length) {
+4            var match = true
+5
+6            for (j in needle.indices) {
+7                if (haystack[i + j] != needle[j]) {
+8                    match = false
+9                    break
+10                }
+11            }
+12
+13            if (match) return i
+14        }
+15
+16        return -1
+17    }
+18}
