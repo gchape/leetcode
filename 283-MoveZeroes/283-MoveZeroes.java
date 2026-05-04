2ffff5f0c1d718b229ae9da4970bd66cc7001900
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 19:18:37 +0400

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,24 @@
+// Last updated: 5/4/2026, 7:18:37 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int z = -1;
+
+        // Find first zero
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                z = i;
+                break;
+            }
+        }
+
+        if (z == -1) return;
+
+        for (int i = z + 1; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[z] = nums[i];
+                nums[i] = 0;
+                z++;
+            }
+        }
+    }
+}
