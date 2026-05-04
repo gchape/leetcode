8508746249119f1a645063647454727cf7b4ef66
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 19:19:06 +0400

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.java
@@ -0,0 +1,38 @@
+// Last updated: 5/4/2026, 7:19:05 PM
+class Solution {
+    public int romanToInt(String s) {
+        int res = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr = value(s.charAt(i));
+
+            if (i < s.length() - 1 && curr < value(s.charAt(i + 1))) {
+                res -= curr;
+            } else {
+                res += curr;
+            }
+        }
+
+        return res;
+    }
+
+    private int value(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
+    }
+}
