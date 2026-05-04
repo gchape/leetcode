9555389dbe236fa47e1a9ac45f3de5cb0fe0e8aa
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 23:53:32 +0400

    File updated at 5/4/2026, 11:53:31 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.kt
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.kt
@@ -1,25 +1,15 @@
-// Last updated: 5/4/2026, 11:14:14 PM
+// Last updated: 5/4/2026, 11:53:31 PM
 1class Solution {
-2    fun strStr(haystack: String, needle: String): Int {
-3        if (needle.isEmpty()) return 0
-4
-5        var i = 0
-6        var j = 0
-7
-8        while (i < haystack.length) {
-9            if (haystack[i] == needle[j]) {
-10                i++
-11                j++
-12
-13                if (j == needle.length) {
-14                    return i - j
-15                }
-16            } else {
-17                i = i - j + 1
-18                j = 0
-19            }
-20        }
-21
-22        return -1
-23    }
-24}
+2    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+3        if (list1 == null) return list2
+4        if (list2 == null) return list1
+5
+6        return if (list1.`val` <= list2.`val`) {
+7            list1.next = mergeTwoLists(list1.next, list2)
+8            list1
+9        } else {
+10            list2.next = mergeTwoLists(list1, list2.next)
+11            list2
+12        }
+13    }
+14}
