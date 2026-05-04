7bb8637631255f826b3532f468ba9387c0584fd1
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 19:18:47 +0400

    Create 164-MaximumGap

---
+++ 164-MaximumGap/164-MaximumGap.java
@@ -0,0 +1,14 @@
+// Last updated: 5/4/2026, 7:18:47 PM
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2)
+            return 0;
+
+        Arrays.sort(nums);
+
+        return IntStream.range(0, nums.length - 1)
+                .map(i -> nums[i + 1] - nums[i])
+                .max()
+                .orElse(0);
+    }
+}
