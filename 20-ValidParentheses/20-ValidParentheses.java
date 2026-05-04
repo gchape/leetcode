ca8d76f4fa49a823a48f3324d8350e9626d7a002
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 19:19:03 +0400

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,21 @@
+// Last updated: 5/4/2026, 7:19:03 PM
+class Solution {
+    public boolean isValid(String s) {
+        var m = Map.of('(', ')', '{', '}', '[', ']');
+        var stack = new ArrayDeque<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (m.containsKey(c)) {
+                stack.push(c);
+            } else if (stack.isEmpty() || m.get(stack.peek()) != c) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
