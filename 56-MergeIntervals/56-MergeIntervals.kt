252c3d8a696ad4e4d1c27531715083fcb69c4c61
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 20:09:05 +0400

    File updated at 5/4/2026, 8:09:04 PM

--- 56-MergeIntervals/56-MergeIntervals.kt
+++ 56-MergeIntervals/56-MergeIntervals.kt
@@ -1,22 +1,30 @@
-// Last updated: 5/4/2026, 7:18:57 PM
-class Solution {
-    fun merge(intervals: Array<IntArray>): Array<IntArray> {
-        if (intervals.isEmpty()) return emptyArray()
-
-        val sorted = intervals.sortedBy { it[0] }
-        val result = mutableListOf(sorted[0])
-
-        for (i in 1..<sorted.size) {
-            val (x1, y1) = result.last()
-            val (x2, y2) = sorted[i]
-
-            if (y1 >= x2) {
-                result[result.size - 1] = intArrayOf(x1, maxOf(y1, y2))
-            } else {
-                result.add(intArrayOf(x2, y2))
-            }
-        }
-
-        return result.toTypedArray()
-    }
-}
+// Last updated: 5/4/2026, 8:09:04 PM
+1class Solution {
+2    fun removeElement(nums: IntArray, `val`: Int): Int {
+3               for ((i, element) in nums.withIndex()) {
+4        if (element == `val`) {
+5            nums[i] = -1
+6        }
+7    }
+8
+9    var j: Int? = null
+10    var i = 0
+11
+12    while (i < nums.size) {
+13        if (nums[i] == -1 && j == null) {
+14            j = i
+15        }
+16
+17        if (nums[i] != -1 && j != null) {
+18            nums[j] = nums[i]
+19            nums[i] = -1
+20            i = j
+21            j = null
+22        }
+23
+24        i++
+25    }
+26
+27    return j ?: nums.size
+28    }
+29}
