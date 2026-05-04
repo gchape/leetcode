a77792ae64e1a7c9eef2f3b35e5c40071881dbfe
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 19:18:36 +0400

    Create 451-SortCharactersByFrequency

---
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -0,0 +1,20 @@
+// Last updated: 5/4/2026, 7:18:36 PM
+class Solution {
+    public String frequencySort(String s) {
+        var map = new HashMap<Character, Integer>();
+
+        for (char c : s.toCharArray()) {
+            map.merge(c, 1, Integer::sum);
+        }
+
+        var list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.<Character, Integer>comparingByValue().reversed());
+
+        var result = new StringBuilder();
+        for (var entry : list) {
+            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+
+        return result.toString();
+    }
+}
