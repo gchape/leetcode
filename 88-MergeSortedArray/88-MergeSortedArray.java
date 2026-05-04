8f06fadb5395e112ef9cde218810b88b7786378a
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-04 19:18:54 +0400

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -0,0 +1,29 @@
+// Last updated: 5/4/2026, 7:18:54 PM
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0, j = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                shiftByOne(nums1, i);
+                nums1[i] = nums2[j];
+                j++;
+                m++;
+            } else {
+                i++;
+            }
+        }
+
+        while (j < n) {
+            nums1[i++] = nums2[j++];
+        }
+    }
+
+    public void shiftByOne(int[] nums1, int from) {
+        for (int i = nums1.length - 1; i > from; i--) {
+            nums1[i] = nums1[i - 1];
+        }
+    }
+}
