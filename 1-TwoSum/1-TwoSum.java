a8974e7b65274f0390de29db8deac0bc6cb11157
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-02 08:03:34 +0400

    File updated at 8/2/2026, 8:03:33 AM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,18 +1,18 @@
-// Last updated: 5/4/2026, 7:19:08 PM
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        var map = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-
-            map.put(nums[i], i);
-        }
-
-        return null;
-    }
-}
+// Last updated: 8/2/2026, 8:03:33 AM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        var map = new HashMap<Integer, Integer>();
+4        for (int i = 0; i < nums.length; i++) {
+5            map.put(nums[i], i);
+6        }
+7
+8        for (int i = 0; i < nums.length; i++) {
+9            var j = map.get(target - nums[i]);
+10            if (j != null && j != i) {
+11                return new int[] { i, j };
+12            }
+13        }
+14
+15        return null;
+16    }
+17}
