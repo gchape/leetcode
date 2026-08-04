f4b6d4803e8ecfb7b6202d05badd27337c2c985e
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-04 11:34:58 +0400

    File updated at 8/4/2026, 11:34:58 AM

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,40 +1,37 @@
-// Last updated: 5/4/2026, 7:19:00 PM
-class Solution {
-    public void nextPermutation(int[] nums) {
-
-        int i = nums.length - 2;
-
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        if (i >= 0) {
-
-            int j = nums.length - 1;
-
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-
-            swap(nums, i, j);
-        }
-
-        reverse(nums, i + 1);
-    }
-
-    private void reverse(int[] nums, int start) {
-
-        int left = start;
-        int right = nums.length - 1;
-
-        while (left < right) {
-            swap(nums, left++, right--);
-        }
-    }
-
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
-}
+// Last updated: 8/4/2026, 11:34:58 AM
+1class Solution {
+2    public void nextPermutation(int[] nums) {
+3        int pivot = -1;
+4
+5        for (int i = nums.length - 1; i > 0; i--) {
+6            if (nums[i - 1] < nums[i]) {
+7                pivot = i - 1;
+8                break;
+9            }
+10        }
+11
+12        if (pivot == -1) {
+13            reverse(0, nums.length, nums);
+14            return;
+15        }
+16
+17        for (int i = nums.length - 1; i > pivot; i--) {
+18            if (nums[i] > nums[pivot]) {
+19                int tmp = nums[i];
+20                nums[i] = nums[pivot];
+21                nums[pivot] = tmp;
+22                break;
+23            }
+24        }
+25
+26        reverse(pivot + 1, nums.length, nums);
+27    }
+28
+29    private void reverse(int from, int to, int[] arr) {
+30        for (int i = 0; i < (to - from) / 2; i++) {
+31            int tmp = arr[from + i];
+32            arr[from + i] = arr[to - i - 1];
+33            arr[to - i - 1] = tmp;
+34        }
+35    }
+36}
