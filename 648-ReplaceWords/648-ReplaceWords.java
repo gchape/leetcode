5702a9eb3a74b3de817f296a4734af8b083bac90
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-05 10:25:17 +0400

    File updated at 8/5/2026, 10:25:17 AM

--- 648-ReplaceWords/648-ReplaceWords.java
+++ 648-ReplaceWords/648-ReplaceWords.java
@@ -1,4 +1,4 @@
-// Last updated: 8/5/2026, 10:22:47 AM
+// Last updated: 8/5/2026, 10:25:17 AM
 1class Solution {
 2    public String replaceWords(List<String> dictionary, String sentence) {
 3        var trie = new Trie();
@@ -8,64 +8,68 @@
 7            trie.insert(word);
 8        }
 9
-10        Arrays.stream(sentence.split(" "))
-11                .forEach(word -> {
-12                    var sb = new StringBuilder();
-13                    for (int i = 0; i < word.length(); i++) {
-14                        sb.append(word.charAt(i));
-15
-16                        if (trie.search(sb.toString())) {
-17                            result.append(sb).append(" ");
-18                            word = null;
-19                            break;
-20                        }
-21                    }
-22                    if (word != null) result.append(word).append(" ");
-23                });
-24
-25        return result.toString().stripTrailing();
-26    }
-27    
-28    public static class Trie {
-29
-30        private final Node root = new Node();
+10        for (String word : sentence.split(" ")) {
+11            StringBuilder prefix = new StringBuilder();
+12            boolean replaced = false;
+13
+14            for (char c : word.toCharArray()) {
+15                prefix.append(c);
+16
+17                if (trie.search(prefix.toString())) {
+18                    result.append(prefix).append(' ');
+19                    replaced = true;
+20                    break;
+21                }
+22            }
+23
+24            if (!replaced) {
+25                result.append(word).append(' ');
+26            }
+27        }
+28        
+29        return result.toString().stripTrailing();
+30    }
 31
-32        public void insert(String word) {
-33            Node node = root;
-34
-35            for (char c : word.toCharArray()) {
-36                node = node.children.computeIfAbsent(c, k -> new Node());
-37            }
+32    public static class Trie {
+33
+34        private final Node root = new Node();
+35
+36        public void insert(String word) {
+37            Node node = root;
 38
-39            node.isWord = true;
-40        }
-41
-42        public boolean search(String word) {
-43            Node node = find(word);
-44            return node != null && node.isWord;
-45        }
-46
-47        public boolean startsWith(String prefix) {
-48            return find(prefix) != null;
+39            for (char c : word.toCharArray()) {
+40                node = node.children.computeIfAbsent(c, k -> new Node());
+41            }
+42
+43            node.isWord = true;
+44        }
+45
+46        public boolean search(String word) {
+47            Node node = find(word);
+48            return node != null && node.isWord;
 49        }
 50
-51        private Node find(String s) {
-52            Node node = root;
-53
-54            for (char c : s.toCharArray()) {
-55                node = node.children.get(c);
-56
-57                if (node == null) {
-58                    return null;
-59                }
-60            }
-61
-62            return node;
-63        }
-64
-65        private static class Node {
-66            Map<Character, Node> children = new HashMap<>();
-67            boolean isWord;
-68        }
-69    }
-70}
+51        public boolean startsWith(String prefix) {
+52            return find(prefix) != null;
+53        }
+54
+55        private Node find(String s) {
+56            Node node = root;
+57
+58            for (char c : s.toCharArray()) {
+59                node = node.children.get(c);
+60
+61                if (node == null) {
+62                    return null;
+63                }
+64            }
+65
+66            return node;
+67        }
+68
+69        private static class Node {
+70            Map<Character, Node> children = new HashMap<>();
+71            boolean isWord;
+72        }
+73    }
+74}
