f3fd50c78f6baf7184dba4ace246268ed48d4964
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-09 22:39:50 +0400

    File updated at 8/9/2026, 10:39:49 PM

--- 162-FindPeakElement/162-FindPeakElement.java
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -1,40 +1,25 @@
-// Last updated: 8/9/2026, 10:34:30 PM
+// Last updated: 8/9/2026, 10:39:49 PM
 1class Solution {
 2    public int findPeakElement(int[] nums) {
-3        if (nums.length == 1) return 0;
+3        int left = 0, right = nums.length - 1;
 4
-5        int left = 0, right = nums.length - 1;
-6
-7        while (left <= right) {
-8            int mid = left + (right - left) / 2;
-9
-10            if (mid == 0) {
-11                if (nums[0] > nums[1]) {
-12                    return 0;
-13                }
+5        while (left <= right) {
+6            int mid = left + (right - left) / 2;
+7
+8            // left neighbor is greater
+9            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+10                right = mid - 1;
+11            }
+12            // right neighbor is greater
+13            else if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) {
 14                left = mid + 1;
-15                continue;
-16            }
-17
-18            if (mid == nums.length - 1) {
-19                if (nums[nums.length - 1] > nums[nums.length - 2]) {
-20                    return nums.length - 1;
-21                }
-22                right = mid - 1;
-23                continue;
-24            }
-25
-26            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-27                return mid;
-28            }
-29
-30            if (nums[mid] < nums[mid + 1]) {
-31                left = mid + 1;
-32            } else {
-33                right = mid - 1;
-34            }
-35        }
-36
-37        return -1;
-38    }
-39}
+15            }
+16            // neither neighbor is greater => peak
+17            else {
+18                return mid;
+19            }
+20        }
+21
+22        return -1;
+23    }
+24}
