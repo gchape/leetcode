a3b09b608f52b4613790d96e88e759eaf0363973
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-12 17:53:38 +0400

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,20 @@
+// Last updated: 8/12/2026, 5:53:38 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
