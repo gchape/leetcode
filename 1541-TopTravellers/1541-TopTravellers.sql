ad8f6c8bfbe2af58d2e9de67731d610360760a12
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-12 17:52:46 +0400

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,11 @@
+-- Last updated: 8/12/2026, 5:52:45 PM
+-- Write your PostgreSQL query statement below
+SELECT
+    u.name,
+    COALESCE(SUM(r.distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY
+    travelled_distance DESC,
+    u.name;
