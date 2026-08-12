48d6f15193a18cc80a81b3dd9e9efb9bb2dedd42
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-12 17:48:24 +0400

    File updated at 8/12/2026, 5:48:23 PM

--- 143-ReorderList/143-ReorderList.java
+++ 143-ReorderList/143-ReorderList.java
@@ -1,44 +1,42 @@
-// Last updated: 8/11/2026, 7:19:55 PM
+// Last updated: 8/12/2026, 5:48:23 PM
 1class Solution {
-2    public void reorderList(ListNode head) {
-3        if (head == null || head.next == null) return;
+2    private ListNode head;
+3    private boolean stop;
 4
-5        // 1. Find middle
-6        ListNode slow = head;
-7        ListNode fast = head;
-8
-9        while (fast.next != null && fast.next.next != null) {
-10            slow = slow.next;
-11            fast = fast.next.next;
-12        }
-13
-14        // 2. Split and reverse second half
-15        ListNode second = slow.next;
-16        slow.next = null;
-17
-18        ListNode prev = null;
-19        while (second != null) {
-20            ListNode next = second.next;
-21            second.next = prev;
-22            prev = second;
-23            second = next;
-24        }
-25
-26        // prev is now the head of reversed second half
-27        second = prev;
-28
-29        // 3. Merge
-30        ListNode first = head;
-31
-32        while (second != null) {
-33            ListNode firstNext = first.next;
-34            ListNode secondNext = second.next;
+5    public void reorderList(ListNode head) {
+6        if (head == null || head.next == null) {
+7            return;
+8        }
+9
+10        this.head = head;
+11        this.stop = false;
+12
+13        reorder(head);
+14    }
+15
+16    private void reorder(ListNode tail) {
+17        if (tail == null) {
+18            return;
+19        }
+20
+21        reorder(tail.next);
+22
+23        if (stop) {
+24            return;
+25        }
+26
+27        // Middle reached
+28        if (head == tail || head.next == tail) {
+29            tail.next = null;
+30            stop = true;
+31            return;
+32        }
+33
+34        ListNode next = head.next;
 35
-36            first.next = second;
-37            second.next = firstNext;
+36        head.next = tail;
+37        tail.next = next;
 38
-39            first = firstNext;
-40            second = secondNext;
-41        }
-42    }
-43}
+39        head = next;
+40    }
+41}
