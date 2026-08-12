6ef4398fca8a4b04c5238f1782b8b76d20b2701f
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-12 18:15:28 +0400

    File updated at 8/12/2026, 6:15:27 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 8/12/2026, 6:11:57 PM
+// Last updated: 8/12/2026, 6:15:27 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,26 +10,16 @@
 9 * }
 10 */
 11class Solution {
-12    private ListNode tail;
-13
-14    public ListNode reverseList(ListNode head) {
-15        if (head == null || head.next == null) return head;
+12    public ListNode reverseList(ListNode head) {
+13        if (head == null || head.next == null) {
+14            return head;
+15        }
 16
-17        head = reverse(head);
-18        head.next = null;
-19
-20        return tail;
-21    }
-22
-23    private ListNode reverse(ListNode head) {
-24        if (head.next == null) {
-25            tail = head;
-26            return head;
-27        }
-28
-29        var prev = reverse(head.next);
-30        prev.next = head;
-31
-32        return head;
-33    }
-34}
+17        ListNode newHead = reverseList(head.next);
+18
+19        head.next.next = head;
+20        head.next = null;
+21
+22        return newHead;
+23    }
+24}
