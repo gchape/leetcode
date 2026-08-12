03c442d4f562f8a5a81ac6a3c0ecfd9f32e3388a
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-12 19:29:52 +0400

    File updated at 8/12/2026, 7:29:51 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.java
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 8/10/2026, 11:37:33 PM
+// Last updated: 8/12/2026, 7:29:51 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -20,9 +20,10 @@
 19    private boolean check(ListNode right) {
 20        if (right == null) return true;
 21        if (!check(right.next)) return false;
-22        if (left.val != right.val) return false;
-23
-24        left = left.next;
-25        return true;
-26    }
-27}
+22        
+23        if (left.val != right.val) return false;
+24
+25        left = left.next;
+26        return true;
+27    }
+28}
