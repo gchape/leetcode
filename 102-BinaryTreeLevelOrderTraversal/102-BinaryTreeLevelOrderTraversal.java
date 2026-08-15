c3eb2c0c1b2e4bab13d6bc676b973649cb204baf
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-15 14:48:13 +0400

    File updated at 8/15/2026, 2:48:12 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,4 +1,4 @@
-// Last updated: 8/15/2026, 2:48:05 PM
+// Last updated: 8/15/2026, 2:48:12 PM
 1class Solution {
 2    public List<List<Integer>> levelOrder(TreeNode root) {
 3        if (root == null) return List.of();
