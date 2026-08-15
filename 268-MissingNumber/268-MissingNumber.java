7e0797f665a0b9d55b11d3f86c912c8b2b305357
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-15 21:21:06 +0400

    File updated at 8/15/2026, 9:21:06 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,12 +1,12 @@
-// Last updated: 8/15/2026, 9:18:14 PM
+// Last updated: 8/15/2026, 9:21:06 PM
 1class Solution {
 2    public int missingNumber(int[] nums) {
-3        var bitSet = new java.util.BitSet(nums.length);
-4
-5        for (var num : nums) {
-6            bitSet.set(num);
+3        int xor = nums.length;
+4        
+5        for (int i = 0; i < nums.length; i++) {
+6            xor ^= i ^ nums[i];
 7        }
-8
-9        return bitSet.nextClearBit(0);
+8        
+9        return xor;
 10    }
 11}
