0cdd383e5e50da60dc4f72aff812433babcd58d7
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-15 13:27:20 +0400

    File updated at 8/15/2026, 1:27:19 PM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -1,39 +1,35 @@
-// Last updated: 8/15/2026, 1:25:07 PM
+// Last updated: 8/15/2026, 1:27:19 PM
 1class Solution {
 2    public int evalRPN(String[] tokens) {
-3        var eval = new ArrayDeque<Integer>();
+3        var stack = new ArrayDeque<Integer>();
 4
 5        for (var token : tokens) {
-6            try {
-7                int digit = Integer.parseInt(token);
-8                eval.push(digit);
-9            } catch (NumberFormatException e) {
-10                int result;
-11                if (eval.size() > 1) {
-12                    int right = eval.pop();
-13                    int left = eval.pop();
-14
-15                    result = switch (token) {
-16                        case "/" -> left / right;
-17                        case "+" -> left + right;
-18                        case "*" -> left * right;
-19                        case "-" -> left - right;
-20                        default -> throw new IllegalStateException("Unexpected value: " + token);
-21                    };
-22
-23                    eval.push(result);
-24                } else {
-25                    result = switch (token) {
-26                        case "+" -> eval.pop();
-27                        case "-" -> -eval.pop();
-28                        default -> throw new IllegalStateException("Unexpected value: " + token);
-29                    };
-30
-31                    eval.push(result);
-32                }
-33            }
-34        }
-35
-36        return eval.pop();
-37    }
-38}
+6            switch (token) {
+7                case "+": {
+8                    stack.push(stack.pop() + stack.pop());
+9                    break;
+10                }
+11                case "-": {
+12                    int right = stack.pop();
+13                    int left = stack.pop();
+14                    stack.push(left - right);
+15                    break;
+16                }
+17                case "*": {
+18                    stack.push(stack.pop() * stack.pop());
+19                    break;
+20                }
+21                case "/": {
+22                    int right = stack.pop();
+23                    int left = stack.pop();
+24                    stack.push(left / right);
+25                    break;
+26                }
+27                default:
+28                    stack.push(Integer.parseInt(token));
+29            }
+30        }
+31
+32        return stack.pop();
+33    }
+34}
