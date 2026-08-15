afbc8cebd4c12c6a8e69a6243c024dd0fc07228c
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-15 14:15:08 +0400

    File updated at 8/15/2026, 2:15:07 PM

--- 71-SimplifyPath/71-SimplifyPath.java
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -1,18 +1,25 @@
-// Last updated: 8/11/2026, 7:33:33 PM
+// Last updated: 8/15/2026, 2:15:07 PM
 1class Solution {
 2    public String simplifyPath(String path) {
-3        var stack = new ArrayDeque<String>();
+3        Deque<String> dirs = new ArrayDeque<>();
 4
-5        for (var part : path.split("/")) {
-6            if (part.isBlank() || part.equals(".")) continue;
-7
-8            if (part.equals("..")) {
-9                if (!stack.isEmpty()) stack.removeLast();
-10            } else {
-11                stack.addLast(part);
-12            }
-13        }
-14
-15        return "/" + String.join("/", stack);
-16    }
-17}
+5        for (var dir : path.split("/")) {
+6            switch (dir) {
+7                case "", "." -> {
+8                }
+9                case ".." -> {
+10                    if (!dirs.isEmpty())
+11                        dirs.pop();
+12                }
+13                default -> dirs.push(dir);
+14            }
+15        }
+16
+17        var joiner = new java.util.StringJoiner("/", "/", "");
+18        while (!dirs.isEmpty()) {
+19            joiner.add(dirs.pollLast());
+20        }
+21
+22        return joiner.toString();
+23    }
+24}
