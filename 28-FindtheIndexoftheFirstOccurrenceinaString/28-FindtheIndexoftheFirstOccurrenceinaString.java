46c775312e94e3e68953478d9c5ea7a511cb3da8
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-19 13:48:11 +0400

    File updated at 8/19/2026, 1:48:11 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,55 +1,23 @@
-// Last updated: 8/19/2026, 1:47:51 PM
+// Last updated: 8/19/2026, 1:48:11 PM
 1class Solution {
 2
 3    public int strStr(String haystack, String needle) {
-4        int n = haystack.length();
-5        int m = needle.length();
-6
-7        if (m == 0)
-8            return 0;
-9        if (m > n)
-10            return -1;
-11
-12        int base = 256;
-13        long mod = 1_000_000_007L;
-14
-15        // Place value of the leading character, e.g. for length m -> base^(m-1)
-16        long highOrder = 1;
-17        for (int i = 0; i < m - 1; i++) {
-18            highOrder = (highOrder * base) % mod;
-19        }
-20
-21        // Hash of the needle
-22        long needleHash = 0;
-23        for (int i = 0; i < m; i++) {
-24            needleHash = (needleHash * base + needle.charAt(i)) % mod;
-25        }
-26
-27        // Hash of the first window in haystack
-28        long windowHash = 0;
-29        for (int i = 0; i < m; i++) {
-30            windowHash = (windowHash * base + haystack.charAt(i)) % mod;
-31        }
-32
-33        int last = n - m;
-34
-35        for (int i = 0; i <= last; i++) {
-36            // Hashes match -> confirm with a real comparison (rules out collisions)
-37            if (windowHash == needleHash && haystack.regionMatches(i, needle, 0, m)) {
-38                return i;
-39            }
-40
-41            if (i < last) {
-42                int leavingChar = haystack.charAt(i);
-43                int enteringChar = haystack.charAt(i + m);
-44
-45                windowHash = ((windowHash - leavingChar * highOrder) * base + enteringChar) % mod;
-46                if (windowHash < 0) {
-47                    windowHash += mod;
-48                }
-49            }
-50        }
-51
-52        return -1;
-53    }
-54}
+4        int last = haystack.length() - needle.length();
+5
+6        for (int i = 0; i <= last; i++) {
+7            boolean found = true;
+8
+9            for (int j = 0; j < needle.length(); j++) {
+10                if (haystack.charAt(i + j) != needle.charAt(j)) {
+11                    found = false;
+12                    break;
+13                }
+14            }
+15
+16            if (found)
+17                return i;
+18        }
+19
+20        return -1;
+21    }
+22}
