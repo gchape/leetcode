166cda0bd274fd160774ea25cd8101b980aa2885
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-19 08:19:50 +0400

    File updated at 8/19/2026, 8:19:50 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.java
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.java
@@ -1,4 +1,4 @@
-// Last updated: 8/19/2026, 8:17:11 AM
+// Last updated: 8/19/2026, 8:19:50 AM
 1import java.text.BreakIterator;
 2
 3class Solution {
@@ -7,16 +7,16 @@
 6        var wordIterator = BreakIterator.getWordInstance();
 7        wordIterator.setText(s);
 8
-9        var sb = new StringBuilder();
-10        int start = wordIterator.first();
-11        for (int end = wordIterator.next(); end != BreakIterator.DONE; start = end, end = wordIterator.next()) {
-12            String token = s.substring(start, end);
-13            if (!token.isBlank()) {
-14                if (sb.length() > 0)
-15                    sb.insert(0, " ");
-16                sb.insert(0, token);
-17            }
-18        }
-19        return sb.toString();
+9        var words = new ArrayDeque<String>(); // or a List you iterate in reverse
+10
+11        int start = wordIterator.first();
+12        for (int end = wordIterator.next(); end != BreakIterator.DONE; start = end, end = wordIterator.next()) {
+13            String token = s.substring(start, end);
+14            if (!token.isBlank()) {
+15                words.addLast(token);
+16            }
+17        }
+18
+19        return String.join(" ", words.reversed()); // Java 21+, or iterate a List backwards
 20    }
 21}
