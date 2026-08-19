353136acd4d56dc56b2d7bde5973c2ed4c31c255
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-19 08:17:11 +0400

    File updated at 8/19/2026, 8:17:11 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.java
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.java
@@ -1,10 +1,22 @@
-// Last updated: 8/4/2026, 10:27:37 PM
-1class Solution {
-2    public String reverseWords(String s) {
-3        String[] words = s.trim().split("\\s+");
+// Last updated: 8/19/2026, 8:17:11 AM
+1import java.text.BreakIterator;
+2
+3class Solution {
 4
-5        Collections.reverse(Arrays.asList(words));
-6
-7        return String.join(" ", words);
-8    }
-9}
+5    public String reverseWords(String s) {
+6        var wordIterator = BreakIterator.getWordInstance();
+7        wordIterator.setText(s);
+8
+9        var sb = new StringBuilder();
+10        int start = wordIterator.first();
+11        for (int end = wordIterator.next(); end != BreakIterator.DONE; start = end, end = wordIterator.next()) {
+12            String token = s.substring(start, end);
+13            if (!token.isBlank()) {
+14                if (sb.length() > 0)
+15                    sb.insert(0, " ");
+16                sb.insert(0, token);
+17            }
+18        }
+19        return sb.toString();
+20    }
+21}
