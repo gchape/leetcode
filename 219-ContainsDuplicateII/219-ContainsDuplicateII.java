ef12ec1850ba2a568ce26d9fe7407a83d655cd31
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-08-20 19:58:32 +0400

    File updated at 8/20/2026, 7:58:32 PM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.java
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -1,21 +1,19 @@
-// Last updated: 8/20/2026, 7:57:23 PM
+// Last updated: 8/20/2026, 7:58:32 PM
 1class Solution {
 2
 3    public boolean containsNearbyDuplicate(int[] nums, int k) {
-4        Map<Integer, Integer> distances = new HashMap<>();
+4        Map<Integer, Integer> lastSeen = new HashMap<>();
 5
 6        for (int i = 0; i < nums.length; i++) {
-7            int num = nums[i];
+7            Integer previous = lastSeen.get(nums[i]);
 8
-9            if (!distances.containsKey(num)) {
-10                distances.put(num, i);
-11            } else if (i - distances.get(num) <= k) {
-12                return true;
-13            } else {
-14                distances.put(num, i);
-15            }
-16        }
-17
-18        return false;
-19    }
-20}
+9            if (previous != null && i - previous <= k) {
+10                return true;
+11            }
+12
+13            lastSeen.put(nums[i], i);
+14        }
+15
+16        return false;
+17    }
+18}
