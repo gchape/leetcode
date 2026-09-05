db66e2671f42bb02040e6dd9e7747db759f1bc13
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-09-05 21:11:37 +0400

    File updated at 9/5/2026, 9:11:36 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,13 +1,17 @@
-// Last updated: 9/5/2026, 9:10:06 PM
+// Last updated: 9/5/2026, 9:11:36 PM
 1class Solution {
 2
 3    public boolean isAnagram(String s, String t) {
-4        var chars1 = s.toCharArray();
-5        Arrays.sort(chars1);
-6
-7        var chars2 = t.toCharArray();
-8        Arrays.sort(chars2);
-9
-10        return new String(chars1).equals(new String(chars2));
-11    }
-12}
+4        if (s.length() != t.length())
+5            return false;
+6        int[] counts = new int[26];
+7        for (int i = 0; i < s.length(); i++) {
+8            counts[s.charAt(i) - 'a']++;
+9            counts[t.charAt(i) - 'a']--;
+10        }
+11        for (int c : counts)
+12            if (c != 0)
+13                return false;
+14        return true;
+15    }
+16}
