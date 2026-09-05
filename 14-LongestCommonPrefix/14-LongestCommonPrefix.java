cba6cc3469db5b126843afff01c3c67bd2e96f15
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-09-05 19:28:16 +0400

    File updated at 9/5/2026, 7:28:16 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -1,18 +1,24 @@
-// Last updated: 5/4/2026, 7:19:04 PM
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
-
-            for (int j = 1; j < strs.length; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != c) {
-                    return strs[0].substring(0, i);
-                }
-            }
-        }
-
-        return strs[0];
-    }
-}
+// Last updated: 9/5/2026, 7:28:16 PM
+1class Solution {
+2
+3    public String longestCommonPrefix(String[] strs) {
+4        var s = strs[0];
+5        var prefix = new StringBuilder();
+6        
+7        for (int i = 0; i < s.length(); i++) {
+8            boolean stop = false;
+9
+10            for (var str : strs) {
+11                if (i >= str.length() || str.charAt(i) != s.charAt(i)) {
+12                    stop = true;
+13                    break;
+14                }
+15            }
+16
+17            if (stop) break;
+18            else prefix.append(s.charAt(i));
+19        }
+20
+21        return prefix.toString();
+22    }
+23}
