8b33131ee3224ed1f4fb81c862402cec9f734995
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-09-06 10:34:11 +0400

    File updated at 9/6/2026, 10:34:11 AM

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,26 +1,35 @@
-// Last updated: 9/6/2026, 10:28:21 AM
+// Last updated: 9/6/2026, 10:34:11 AM
 1class Solution {
 2
 3    public List<List<Integer>> threeSum(int[] nums) {
-4        Set<List<Integer>> uniqueTriplets = new HashSet<>();
-5
-6        for (int i = 0; i < nums.length; i++) {
-7            int target = -nums[i];
-8            Set<Integer> seen = new HashSet<>();
-9
-10            for (int j = i + 1; j < nums.length; j++) {
-11                int complement = target - nums[j];
-12
-13                if (seen.contains(complement)) {
-14                    List<Integer> triplet = new ArrayList<>(List.of(nums[i], complement, nums[j]));
-15                    Collections.sort(triplet);
-16                    uniqueTriplets.add(triplet);
-17                }
-18
-19                seen.add(nums[j]);
-20            }
-21        }
-22
-23        return new ArrayList<>(uniqueTriplets);
-24    }
-25}
+4        Arrays.sort(nums);
+5        List<List<Integer>> result = new ArrayList<>();
+6
+7        for (int i = 0; i < nums.length - 2; i++) {
+8            if (i > 0 && nums[i] == nums[i - 1])
+9                continue; // skip duplicate 'i' values
+10
+11            int left = i + 1, right = nums.length - 1;
+12            int target = -nums[i];
+13
+14            while (left < right) {
+15                int sum = nums[left] + nums[right];
+16                if (sum == target) {
+17                    result.add(List.of(nums[i], nums[left], nums[right]));
+18                    left++;
+19                    right--;
+20                    while (left < right && nums[left] == nums[left - 1])
+21                        left++; // skip duplicate 'left' values
+22                    while (left < right && nums[right] == nums[right + 1])
+23                        right--; // skip duplicate 'right' values
+24                } else if (sum < target) {
+25                    left++;
+26                } else {
+27                    right--;
+28                }
+29            }
+30        }
+31
+32        return result;
+33    }
+34}
